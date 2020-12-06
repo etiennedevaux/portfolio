8ac8d858ae0a2ddd81c1e085d260f810ff8f592a
Stephen Clarke <60775095+etiennedevaux@users.noreply.github.com>
2020-12-06 10:34:13 +0000

    Add files via upload
    First draft of the Weeping Ash project

---
+++ weepingash/js/weepingash.js
@@ -0,0 +1,11 @@
+function showMenuComponent(sect) {
+var elements = document.getElementsByClassName("MenuComponent");
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = "none";
+    }
+var x = document.getElementById(sect);
+x.style.display = "block";
+document.getElementById("SerNo").textContent="."+jsfileversion;
+}
+
