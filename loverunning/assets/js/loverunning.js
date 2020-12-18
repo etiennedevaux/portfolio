7edef2bf427a9f752c5e6e7f4d3f70b4e14f699e
Stephen Clarke <60775095+etiennedevaux@users.noreply.github.com>
2020-12-18 05:31:06 +0000

    Add files via upload
    Version of the CodeInstitute Love Running project.  Meets core requirements for graphics, layout, responsiveness with additional PHP and links to SQL server.  Also footer reformatted with work to do on automating the content of the footer

---
+++ loverunning/assets/js/loverunning.js
@@ -0,0 +1,28 @@
+// Global Variables
+var jsfileversion="0001";
+function showMenuComponent(sect) {
+var elements = document.getElementsByClassName("MenuComponent");
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = "none";
+    }
+var x = document.getElementById(sect);
+x.style.display = "block";
+
+var elements = document.getElementsByClassName("MenuButton");
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.textDecoration = "";
+    }
+
+var y = document.getElementById(sect + "Button");
+y.style.textDecoration = "underline";
+
+document.getElementById("JSSerNo").textContent="."+jsfileversion;
+}
+
+function formResponse(){
+document.getElementById('form-response').style.display='inline';
+document.getElementById('join-button').style.display='none';
+}
+
